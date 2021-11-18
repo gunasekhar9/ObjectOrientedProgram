f2113200dd896923eb8d1e5dce23310a9e548fed
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-18 12:26:23 +0530

    [gunasekhar9] add : ObjectOrientedProgram

---
+++ ObjectOrientedProgram/ObjectOrientedProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Object Oriented Programs");
+                Console.WriteLine("Choose the Option : \n 1.JSON \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InventoryMain inv = new InventoryMain();
+                        inv.DisplayData(@"C:\Users\gunas\OneDrive\Documents\Git Problems\ObjectOrientedProgram\ObjectOrientedProgram\ObjectOrientedProgram\Files\Inventory.json");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
