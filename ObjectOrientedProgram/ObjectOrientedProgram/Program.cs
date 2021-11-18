d3a60ffd917590d1392e4419f19747452a59ef27
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-18 14:00:55 +0530

    [gunasekhar9] add : Stock Management is Created

--- ObjectOrientedProgram/ObjectOrientedProgram/Program.cs
+++ ObjectOrientedProgram/ObjectOrientedProgram/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ObjectOrientedProgram;
+using System;
 
 namespace ObjectOrientedProgram
 {
@@ -10,7 +11,7 @@ namespace ObjectOrientedProgram
             while (flag)
             {
                 Console.WriteLine("Welcome to Object Oriented Program");
-                Console.WriteLine("Choose the Option : \n 1. JSON \n 2. Inventory Management \n 3. Exit");
+                Console.WriteLine("Choose the Option : \n 1. JSON \n 2. Inventory Management \n 3. Stock List \n 4. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -26,6 +27,11 @@ namespace ObjectOrientedProgram
                         inven.DisplayData("Pulses");
                         break;
                     case 3:
+                        StockMain stock = new StockMain();
+                        Console.WriteLine("The Stock Report is : ");
+                        stock.DisplayData(@"C:\Users\gunas\OneDrive\Documents\Git Problems\ObjectOrientedProgram\ObjectOrientedProgram\ObjectOrientedProgram\Files\Stock.json");
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
